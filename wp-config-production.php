7627ac1e3f248eed1a7f4e97838f567d1e078bbc
Juppir Eds <eds@juppir.com>
2014-06-18 13:51:37 +0800

    created config files

---
+++ wp-config-production.php
@@ -0,0 +1,20 @@
+<?php
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define('DB_NAME', 'wordpress');^M
+
+/** MySQL database username */
+define('DB_USER', 'wp');^M
+
+/** MySQL database password */
+define('DB_PASSWORD', 'UnaGd9');^M
+
+/** MySQL hostname */
+define('DB_HOST', 'localhost');^M
+
+/** Database Charset to use in creating database tables. */
+define('DB_CHARSET', 'utf8');
+
+/** The Database Collate type. Don't change this if in doubt. */
+define('DB_COLLATE', '');
+
